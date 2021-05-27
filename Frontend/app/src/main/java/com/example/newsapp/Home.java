5f71ac35daed0c03cf34b9c75809cde7a42e138e
Musharraf-02 <musharraf4102@gmail.com>
2021-05-28 00:01:52 +0500

    home screen added

---
+++ Frontend/app/src/main/java/com/example/newsapp/Home.java
@@ -0,0 +1,42 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Home extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    List<News> newsList = new ArrayList<News>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        setListAdapter();
+    }
+    private void setListAdapter() {
+        News n1 = new News("Lorem Ipsum is simply dummy text of the printing and typesetting industry."," Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.", "Source", "27-5-2021");
+        News n2 = new News("Lorem Ipsum is simply dummy text of the printing and typesetting industry."," Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.", "Source", "27-5-2021");
+        News n3 = new News("Lorem Ipsum is simply dummy text of the printing and typesetting industry."," Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.", "Source", "27-5-2021");
+        News n4 = new News("Lorem Ipsum is simply dummy text of the printing and typesetting industry."," Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.", "Source", "27-5-2021");
+        News n5 = new News("Lorem Ipsum is simply dummy text of the printing and typesetting industry."," Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.", "Source", "27-5-2021");
+        News n6 = new News("Lorem Ipsum is simply dummy text of the printing and typesetting industry."," Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.", "Source", "27-5-2021");
+
+        newsList.addAll(Arrays.asList(new News[]{n1,n2,n3,n4,n5,n6}));
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new RecyclerViewAdapter(newsList,Home.this) {
+
+        };
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ Frontend/app/src/main/java/com/example/newsapp/RecyclerViewAdapter.java
@@ -0,0 +1,63 @@
+package com.example.newsapp;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>{
+    List<News> newsList;
+    Activity mAct;
+
+    public RecyclerViewAdapter(List<News> news, Activity mAct) {
+        this.newsList = news;
+        this.mAct = mAct;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.news, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.data=newsList.get(position);
+        holder.textViewTitle.setText(holder.data.getTitle());
+        holder.textViewDescription.setText(holder.data.getDescription());
+        holder.textViewSource.setText(holder.data.getSource());
+        holder.textViewDate.setText(holder.data.getDate());
+        holder.imageViewNews.setImageResource(R.drawable.bbc);
+    }
+
+    @Override
+    public int getItemCount() {
+        return newsList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageViewNews;
+        TextView textViewTitle;
+        TextView textViewDescription;
+        TextView textViewSource;
+        TextView textViewDate;
+        News data;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewNews = itemView.findViewById(R.id.newsImage);
+            textViewTitle = itemView.findViewById(R.id.newsTitle);
+            textViewDescription = itemView.findViewById(R.id.newsDescription);
+            textViewSource = itemView.findViewById(R.id.newsSource);
+            textViewDate = itemView.findViewById(R.id.newsDate);
+        }
+    }
+}
